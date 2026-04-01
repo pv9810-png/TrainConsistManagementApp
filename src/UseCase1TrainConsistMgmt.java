4b2e0710f5dbb8bb1d1ee4c28b7d1d1814b3d817
pv9810-png <pv9810@srmist.edu.in>
2026-04-01 08:30:51 +0530

    UC1

---
+++ src/UseCase1TrainConsistMgmt.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class UseCase1TrainConsistMgmt {
+
+    public static void main(String[] args) {
+        System.out.println("=======================================");
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("=======================================\n");
+        List<String> trainConsist = new ArrayList<>();
+        System.out.println("Train initialized successfully...");
+        System.out.println("Initial number of bogies: " + trainConsist.size());
+        System.out.println("Current train consist: " + trainConsist);
+    }
+}
